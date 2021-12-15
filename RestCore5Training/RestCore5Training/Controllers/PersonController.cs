5d4fd354ccda9bb6ced92426836a3a3ac3858610
Claudemir Bezerra <claudemir.bezerraguinho@gmail.com>
2021-12-15 00:56:30 -0400

    Creating person class and person controller

---
+++ RestCore5Training/RestCore5Training/Controllers/PersonController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RestCore5Training.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("ok");
+        }
+    }
+}
